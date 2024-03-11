c2be5880cb8d010aa2ee8d9fccbfa9959185c8ec
lallue <alluel@3il.fr>
2024-03-11 11:57:29 +0100

    ajout pour la visualisation du jeu

--- src/fr/ecole3il/rodez2023/visualisateur/ApplicationJeuDuPendu.java
+++ src/fr/ecole3il/rodez2023/visualisateur/ApplicationJeuDuPendu.java
@@ -4,6 +4,7 @@ package fr.ecole3il.rodez2023.visualisateur;
 import fr.ecole3il.rodez2023.jeu.AffichagePendu;
 import fr.ecole3il.rodez2023.jeu.JeuDuPenduUI;
 import fr.ecole3il.rodez2023.mots.GestionnaireJeu;
+import fr.ecole3il.rodez2023.mots.GestionnaireLettre;
 import fr.ecole3il.rodez2023.mots.LecteurMots;
 
 import java.util.*;
@@ -20,7 +21,8 @@ public class ApplicationJeuDuPendu {
                 List<String> motsList = lecteurMots.getMotsList();
                 GestionnaireJeu gestionnaireJeu = new GestionnaireJeu(motsList);
                 AffichagePendu affichagePendu = new AffichagePendu();
-                JeuDuPenduUI jeuDuPenduUI = new JeuDuPenduUI(gestionnaireJeu, affichagePendu);
+                GestionnaireLettre gestionnaireLettre = new GestionnaireLettre(gestionnaireJeu);
+                JeuDuPenduUI jeuDuPenduUI = new JeuDuPenduUI(gestionnaireJeu, affichagePendu, gestionnaireLettre);
                 jeuDuPenduUI.setVisible(true);
             }
         });
