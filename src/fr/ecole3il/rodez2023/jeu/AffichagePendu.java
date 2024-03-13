9cf4638a8b565572c48b2be30d7852fa76d1c403
lallue <alluel@3il.fr>
2024-03-13 14:11:03 +0100

    affichage de tous le pendue

--- src/fr/ecole3il/rodez2023/jeu/AffichagePendu.java
+++ src/fr/ecole3il/rodez2023/jeu/AffichagePendu.java
@@ -17,6 +17,11 @@ public class AffichagePendu extends JPanel {
         repaint();
     }
 
+    public void reset() {
+        erreurs = 0;
+        repaint();
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -25,29 +30,37 @@ public class AffichagePendu extends JPanel {
 
     private void dessinerPendu(Graphics g) {
         if (erreurs >= 1) {
-            // Tête
-            g.drawOval(50, 50, 50, 50);
+            // Potence verticale
+            g.drawLine(100, 50, 100, 300);
         }
         if (erreurs >= 2) {
-            // Corps
-            g.drawLine(75, 100, 75, 200);
+            // Barre horizontale
+            g.drawLine(100, 50, 250, 50);
         }
         if (erreurs >= 3) {
-            // Bras gauche
-            g.drawLine(75, 120, 50, 150);
+            // Tête
+        	g.drawLine(250, 50, 250, 100);
+            g.drawOval(225, 100, 50, 50);
         }
         if (erreurs >= 4) {
-            // Bras droit
-            g.drawLine(75, 120, 100, 150);
+            // Corps
+            g.drawLine(250, 150, 250, 250);
         }
         if (erreurs >= 5) {
-            // Jambe gauche
-            g.drawLine(75, 200, 50, 250);
+            // Bras gauche
+            g.drawLine(250, 150, 225, 200);
         }
         if (erreurs >= 6) {
+            // Bras droit
+            g.drawLine(250, 150, 275, 200);
+        }
+        if (erreurs >= 7) {
+            // Jambe gauche
+            g.drawLine(250, 250, 225, 300);
+        }
+        if (erreurs >= 8) {
             // Jambe droite
-            g.drawLine(75, 200, 100, 250);
+            g.drawLine(250, 250, 275, 300);
         }
     }
 }
-
